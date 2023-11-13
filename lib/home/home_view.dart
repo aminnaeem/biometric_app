ddf7d905fe5b5f146856b89a21cc8e05ac0719d6
aminnaeem <amin.naeem1233@gmail.com>
2023-11-13 23:10:32 +0500

    updating home screen

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -6,6 +6,15 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.only(bottomRight: Radius.circular(400))
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text(
           'Home View'
